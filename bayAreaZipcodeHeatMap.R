6f4d3bbac78773d486b4a36f5beb8fa96b6923da
AJ Heller <aj@drfloob.com>
2016-04-18 12:53:11 -0700

    finish associating zips with crimes; simplify filtering by crime description

--- bayAreaZipcodeHeatMap.R
+++ bayAreaZipcodeHeatMap.R
@@ -1,103 +1,105 @@
 
-
 library(gdata)
 library(jsonlite)
 library(RCurl)
 library(tidyr)
+library(data.table)
+library(stringr)
+
 data("zipcode")
 
 
 # Parse zillow data by zip code
 fn <- "zillowData.byZip.dump"
 if(!file.exists(fn)) {
-  zdatafn <- "zdata/Zip_Zhvi_SingleFamilyResidence.csv"
-  zdataurl <- "http://files.zillowstatic.com/research/public/Zip/Zip_Zhvi_SingleFamilyResidence.csv"
-  if (!file.exists(zdatafn)) {
-    download.file(zdataurl, zdatafn)
-  }
-  csv <- read.csv(zdatafn)
-  sf <- csv[csv$Metro == "San Francisco" & csv$City %in% c("Oakland", "San Francisco", "San Mateo") & csv$X2016.02 < 900000,]
-  sfsimp <- sf[,c("City", "RegionName", "X2016.02")]
-  
+    zdatafn <- "zdata/Zip_Zhvi_SingleFamilyResidence.csv"
+    zdataurl <- "http://files.zillowstatic.com/research/public/Zip/Zip_Zhvi_SingleFamilyResidence.csv"
+    if (!file.exists(zdatafn)) {
+        download.file(zdataurl, zdatafn)
+    }
+    csv <- read.csv(zdatafn)
+    sf <- csv[csv$Metro == "San Francisco" & csv$City %in% c("Oakland", "San Francisco", "San Mateo") & csv$X2016.02 < 900000,]
+    sfsimp <- sf[,c("City", "RegionName", "X2016.02")]
+    
 } else {
-  dump(c('sf', 'sfsimp'), fn)
-  source(fn)
+    dump(c('sf', 'sfsimp'), fn)
+    source(fn)
 }
 
 # pull google data
 gfn <- "gData.dump"
 if (!file.exists(gfn)) {
-  credFile <- "credentials.R"
-  if (!file.exists(credFile)) {
-    stop("Missing credentials file: credentials.R. See README.md for formatting")
-  }
-  
-  source(credFile)
-  url <- function(zip) {
-    root <- "https://maps.googleapis.com/maps/api/directions/json?"
-    u <- paste(sep="", 
-               root, 
-               "origin=", zip,
-               "&destination=225 Bush St, San Francisco, CA",
-               "&mode=transit",
-               "&key=", apiKey)
-    URLencode(u)
-  }
-  gdata <- list()
-  
-  rn <- sfsimp$RegionName
-  for (i in seq_along(rn)) {
+    credFile <- "credentials.R"
+    if (!file.exists(credFile)) {
+        stop("Missing credentials file: credentials.R. See README.md for formatting")
+    }
     
-    print(rn[i])
-    doc <- fromJSON(url(rn[i]))
-    gdata[[as.character(rn[i])]] <- doc
-  }
-  
-  dump(c('gdata'), gfn)
+    source(credFile)
+    url <- function(zip) {
+        root <- "https://maps.googleapis.com/maps/api/directions/json?"
+        u <- paste(sep="", 
+                   root, 
+                   "origin=", zip,
+                   "&destination=225 Bush St, San Francisco, CA",
+                   "&mode=transit",
+                   "&key=", apiKey)
+        URLencode(u)
+    }
+    gdata <- list()
+    
+    rn <- sfsimp$RegionName
+    for (i in seq_along(rn)) {
+        
+        print(rn[i])
+        doc <- fromJSON(url(rn[i]))
+        gdata[[as.character(rn[i])]] <- doc
+    }
+    
+    dump(c('gdata'), gfn)
 } else {
 }
-  source(gfn)
+source(gfn)
 
 
 avgRouteDuration <- function(routes, i) {
-  Reduce(function(s, l) {
-    v = l$duration$value
-    if(!is.null(v)) {
-      s <- s + v
-    }
-    s
-  }, routes[i,"legs"], 0)
-
+    Reduce(function(s, l) {
+        v = l$duration$value
+        if(!is.null(v)) {
+            s <- s + v
+        }
+        s
+    }, routes[i,"legs"], 0)
+    
 }
 routefn <- "gdata.routes.dump"
 if (!file.exists(routefn)) {
-  distances <- data.frame(zip=numeric(), distance=numeric())
-  # avg minutes per trip
-  for (n in names(gdata)) {
-    vname <- paste(sep="", "g", n)
-    fn <- paste(sep="", "gdata/", n, ".dump")
-    if (file.exists(fn)) {
-      source(fn)
-    } else {
-      runsum <- 0
-      g <- gdata[[n]]
-      runcnt <- 0
-      # dput(list('nrow routes', nrow(g$routes)))
-      for (r in 1:nrow(g$routes)) {
-        s <- avgRouteDuration(g$routes, r)
-        runsum <- runsum + s
-        runcnt <- runcnt + 1
-      }
-      # dput(list(n, sprintf("%0.2f", runsum/runcnt/60)))
-      assign(vname, list(zip=as.numeric(n), distance=runsum/runcnt/60))
-      dump(vname, file=fn)
+    distances <- data.frame(zip=numeric(), distance=numeric())
+    # avg minutes per trip
+    for (n in names(gdata)) {
+        vname <- paste(sep="", "g", n)
+        fn <- paste(sep="", "gdata/", n, ".dump")
+        if (file.exists(fn)) {
+            source(fn)
+        } else {
+            runsum <- 0
+            g <- gdata[[n]]
+            runcnt <- 0
+            # dput(list('nrow routes', nrow(g$routes)))
+            for (r in 1:nrow(g$routes)) {
+                s <- avgRouteDuration(g$routes, r)
+                runsum <- runsum + s
+                runcnt <- runcnt + 1
+            }
+            # dput(list(n, sprintf("%0.2f", runsum/runcnt/60)))
+            assign(vname, list(zip=as.numeric(n), distance=runsum/runcnt/60))
+            dump(vname, file=fn)
+        }
+        
+        distances <- rbind(distances, get(vname))
     }
-
-    distances <- rbind(distances, get(vname))
-  }
-  dump(c('distances'), routefn)
+    dump(c('distances'), routefn)
 } else {
-  source(routefn)
+    source(routefn)
 }
 
 
@@ -107,12 +109,11 @@ final <- cbind(distances, midCost=sfsimp[sfsimp$RegionName == distances$zip, "X2
 commonLimits <- "$limit=500000&$where=%s between '2015-01-01T00:00:00.000' and '2016-03-31T23:59:59.999'"
 
 pullCrime <- function(url, fn, dt) {
-  l <- sprintf(commonLimits, dt)
-  if (!file.exists(fn)) {
-    download.file(paste(sep="", url, l), fn)
-  }
-  read.csv(fn)
-  
+    l <- sprintf(commonLimits, dt)
+    if (!file.exists(fn)) {
+        download.file(paste(sep="", url, l), fn)
+    }
+    read.csv(fn, as.is = c("location_1", "location", "block_location"))
 }
 
 # SF crime API
@@ -138,33 +139,37 @@ berk <- pullCrime(berkcrimeurl, berkcrimefn, "eventdt")
 # San Mateo, Daly City, etc crime
 # NOPE!
 
-crimDescGrepPattern <- "kidnap|weapons|violent|firearm|robbery|assault|homicide|stolen vehicle|vehicle theft"
-# accCrimeLevels <- levels(acc$crimedescription)[grep(crimDescGrepPattern, levels(acc$crimedescription), ignore.case = T)]
-# sfcCrimeLevels <- levels(sfc$descript)[grep(crimDescGrepPattern, levels(sfc$descript), ignore.case = T)]
-#oakCrimeLevels <- levels(sfc$descript)[grep(crimDescGrepPattern, levels(sfc$descript), ignore.case = T)]
 
-getRelevantIndexes <- function(...) {
-  v <- list(...)
-  # dput(list('v', length(v), class(v)))
-  unlist(lapply(v, function(x) {
-    grep(crimDescGrepPattern, x, ignore.case = T)
-  }))
+# Unified descriptions
+sfc['d'] <- sfc$descript
+acc['d'] <- acc$crimedescription
+oak['d'] <- oak$description
+berk['d'] <- berk$offense
+
+
+crimDescGrepPattern <- "kidnap|weapons|violent|firearm|robbery|assault|homicide|stolen vehicle|vehicle theft"
+## Multi-column search version
+# getRelevantIndexes <- function(...) {
+#   v <- list(...)
+#   # dput(list('v', length(v), class(v)))
+#   unlist(lapply(v, function(x) {
+#     grep(crimDescGrepPattern, x, ignore.case = T)
+#   }))
+# }
+getRelevantIndexes <- function(col) {
+    grep(crimDescGrepPattern, col, ignore.case = T)
 }
 
-# accImpCrimes <- acc[acc$crimedescription %in% accCrimeLevels,]
-# sfcImpCrimes <- sfc[sfc$descript %in% sfcCrimeLevels,]
+
+
+
 
 print("filtering crimes")
 
-# sfcImpCrimes <- sfc[getRelevantIndexes(sfc$descript, sfc$category),]
-# accImpCrimes <- acc[getRelevantIndexes(acc$crimedescription),]
-# oakImpCrimes <- oak[getRelevantIndexes(oak$crimetype, oak$description),]
-# berkImpCrimes <- berk[getRelevantIndexes(berk$cvlegend, berk$offense),]
-
-sfcImpCrimes <- sfc[getRelevantIndexes(sfc$descript),]
-accImpCrimes <- acc[getRelevantIndexes(acc$crimedescription),]
-oakImpCrimes <- oak[getRelevantIndexes(oak$description),]
-berkImpCrimes <- berk[getRelevantIndexes(berk$offense),]
+sfcImpCrimes <- sfc[getRelevantIndexes(sfc$d),]
+accImpCrimes <- acc[getRelevantIndexes(acc$d),]
+oakImpCrimes <- oak[getRelevantIndexes(oak$d),]
+berkImpCrimes <- berk[getRelevantIndexes(berk$d),]
 
 
 
@@ -175,9 +180,11 @@ berkImpCrimes <- berk[getRelevantIndexes(berk$offense),]
 
 
 getLatLon <- function(ds, var) {
-  re <- "([-\\.[:alnum:]]+)\\s([-\\.[:alnum:]]+)"
-  ds %>% extract_(var, c("longitude", "latitude"), regex=re, remove = FALSE, convert = TRUE)
+    re <- "([-\\.[:alnum:]]+)\\s([-\\.[:alnum:]]+)"
+    ds %>% extract_(var, c("longitude", "latitude"), regex=re, remove = FALSE, convert = TRUE)
 }
+
+print("parsting latitude / longitude")
 
 sfcImpLL <- getLatLon(sfcImpCrimes, "location")
 accImpLL <- getLatLon(accImpCrimes, "location_1")
@@ -186,14 +193,30 @@ berkImpLL <- getLatLon(berkImpCrimes, "block_location")
 
 zc <- data.table(zipcode, key=c("latitude", "longitude"))
 sfcdt <- data.table(sfcImpLL, key=c("latitude", "longitude"))
+accdt <- data.table(accImpLL, key=c("latitude", "longitude"))
+oakcdt <- data.table(oakImpLL, key=c("latitude", "longitude"))
+berkcdt <- data.table(berkImpLL, key=c("latitude", "longitude"))
 
-lstSqrz <- function(u,v) {
-  vec <- with(zc, (u-longitude)^2 + (v-latitude)^2)
-  zc[which.min(vec),]$zip
+getZipViaLstSqrz <- function(lng,lat) {
+    vec <- with(zc, (lng-longitude)^2 + (lat-latitude)^2)
+    zc[which.min(vec),]$zip
 }
-transform(sfcdt, zip=apply(sfcdt, 1, function(u) {
-  # dput(list('u', class(u), names(u), u[['longitude']]))
-  lstSqrz(as.numeric(u[['longitude']]), as.numeric(u[['latitude']]))
-  }))
+zipCrime <- function(dt) {
+    dt %>% rowwise() %>% mutate(zip=getZipViaLstSqrz(longitude, latitude))
+}
 
+
+print("finding zip codes")
+
+sfCrimeZipped <- zipCrime(sfcdt)
+acCrimeZipped <- zipCrime(accdt)
+oakCrimeZipped <- zipCrime(oakcdt)
+berkCrimeZipped <- zipCrime(berkcdt)
+
+print("summing crimes by zip")
+
+sfCrimeCount <- sfCrimeZipped[,list(count=sum(!is.na(d))), by=zip]
+acCrimeCount <- acCrimeZipped[,list(count=sum(!is.na(d))), by=zip]
+oakCrimeCount <- oakCrimeZipped[,list(count=sum(!is.na(d))), by=zip]
+berkCrimeCount <- berkCrimeZipped[,list(count=sum(!is.na(d))), by=zip]
 
