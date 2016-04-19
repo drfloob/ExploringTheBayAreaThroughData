01e7373c48392a66605b68d039d61b0434295618
AJ Heller <aj@drfloob.com>
2016-04-19 13:53:26 -0700

    add script that assists zipcode shape file gathering (reproducible research)
    It relies on a website that goes out of its way to make scripted downloads
    difficult (not impossible), but I'd rather not circumvent. It also relies on an
    external conversion tool gdal. I could probably run it from R, but for Win/Mac
    users, I'd rather just tell them what's needed and let them figure it
    out. User-friendliness available upon request.

---
+++ gatherZipcodeShapes.R
@@ -0,0 +1,23 @@
+zipDir <- "zipGeo"
+if (!dir.exists(zipDir)) create.dir(zipDir)
+
+zipFile <- file.path(zipDir, "tl_2010_06_zcta510.zip")
+shapeFilename <- "tl_2010_06_zcta510.shp"
+
+if (!file.exists(file.path(zipDir, shapeFilename))) {
+
+    if (!file.exists(zipFile)) {
+        stop("Zip shape data is not available.",
+             "Download the 2010 California Zipcode data set from https://www.census.gov/cgi-bin/geo/shapefiles2010/main ",
+             "and place it in the ", zipDir, " folder")
+    }
+
+    unzip(zipfile = zipFile, exdir = zipDir)
+}
+
+print("The next step requires you to install the GDAL tool: http://www.gdal.org/")
+print("At a command line, execute:")
+
+outfile <- file.path(getwd(), zipDir, "zcta5.json")
+infile <- file.path(getwd(), zipDir, shapeFilename)
+print(sprintf("ogr2ogr -f \"GeoJSON\" %s %s", outfile, infile), quote = F)
